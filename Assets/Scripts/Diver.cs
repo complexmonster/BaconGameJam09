237f48f76472e9d546c1d5656658231c260549ec
devPhil <philippgullberg@gmail.com>
2015-06-14 16:48:58 +1000

    v0.7
    - Treasure capturing
    - Can't remember what I've worked on
    - Fish
    - Shop art
    - Nitrogen poisoning
    - Brain Fried

--- Assets/Scripts/Diver.cs
+++ Assets/Scripts/Diver.cs
@@ -3,45 +3,105 @@ using System.Collections;
 
 public class Diver : MonoBehaviour {
 
+    public static Diver diver;
+
     public float startDepth;
     public float nitrogen = 0;
-    public float weight = 0;
+    public float energy = 100;
+    public float maxEnergy = 100;
+    public float energyConsumption = 10f;
+    public float energyBoost = 10f;
+    public float energyGain = 1f;
     public float swimPower = 200f;
-    public int floatBags = 1;
+    public int liftingBags = 1;
+    public int cashMoney = 0;
 
     private float currentDepth;
-
 
     private float timeStart = 0f;
     private float timeDiveTotal = 0f;
 
-    public UnityEngine.UI.Text depthText;
+    private bool readyForTreasure = false;
+
+    public UnityEngine.UI.Text depthText, cashText;
     public UnityEngine.UI.Image weightBar, nitrogenBar;
+    public GameObject bagPrefab, interactUI;
 
     private float degree, angle;
 
-	// Use this for initialization
-	void Start () {
-        startDepth = transform.position.y;
+    void Awake ()
+    {
+        if (diver == null) diver = this;
+    }
 
+	// Use this for initialization
+	void Start () 
+    {
+        startDepth = transform.position.y;
         timeStart = Time.time;
+
+        GameManager.Tick += UpdateNitrogenLevels;
+        GameManager.Tick += UpdateEnergyLevels;
 	}
+
+    public void ReadyTreasure (GameObject _treasure)
+    {
+        if (liftingBags >= 1)
+        {
+            //interactUI.SetActive(true);
+
+            if (readyForTreasure && !_treasure.GetComponent<Treasure>().hasBeenPickedUp) ApplyTreasureBag(_treasure);
+        }
+    }
+
+    void ApplyTreasureBag (GameObject _treasure)
+    {
+        GameObject tempBag = (GameObject) Instantiate(bagPrefab, new Vector3(_treasure.transform.position.x, _treasure.transform.position.y+1, 0), Quaternion.identity);
+        tempBag.GetComponent<DistanceJoint2D>().connectedBody = _treasure.GetComponent<Rigidbody2D>();
+
+        _treasure.GetComponent<Treasure>().SetLiftBag(tempBag);
+        _treasure.GetComponent<BoxCollider2D>().enabled = false;
+
+        liftingBags -= 1;
+    }
+
+    public void PayTheMan (int _money)
+    {
+        cashMoney += _money;
+        cashText.gameObject.GetComponent<Animator>().Play("Cash");
+    }
+
+    void UpdateNitrogenLevels()
+    {
+        if (nitrogen > 100f) { print("DEAD AT:" + Time.time); GameManager.gm.GameObver(); }
+
+        if (Mathf.Abs(currentDepth) > 1f)
+        {
+            nitrogen += Mathf.Abs(currentDepth) / 10;
+        }
+    }
+
+    void UpdateEnergyLevels ()
+    {
+        if (energy + energyGain < maxEnergy) energy += energyGain;
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
+        // UPDATE VALUES
         currentDepth = transform.position.y - startDepth;
-
         timeDiveTotal = Time.time - timeStart;
-
-        depthText.text = Mathf.Abs(Mathf.RoundToInt(currentDepth)).ToString();
 
         UpdateUI();
 	}
 
     void UpdateUI ()
     {
-        weightBar.gameObject.GetComponent<StatusBar>().UpdateBar(weight, 100);
+        depthText.text = Mathf.Abs(Mathf.RoundToInt(currentDepth)).ToString();
+        weightBar.gameObject.GetComponent<StatusBar>().UpdateBar(energy, 100);
         nitrogenBar.gameObject.GetComponent<StatusBar>().UpdateBar(nitrogen, 100);
+        cashText.text = "$"+cashMoney.ToString();
     }
 
     void FixedUpdate()
@@ -75,22 +135,34 @@ public class Diver : MonoBehaviour {
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(10 * Time.deltaTime, 0), ForceMode2D.Impulse);
                 angle = 15f;
             }
+
+            if (Input.GetKey(KeyCode.S) && energy >= energyConsumption)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -energyBoost * Time.deltaTime), ForceMode2D.Impulse);
+                energy -= energyConsumption * Time.deltaTime;
+            }
         }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle), Time.deltaTime * 10);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            readyForTreasure = true;
+            interactUI.SetActive(false);
+        }
+        else readyForTreasure = false;
 
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle), Time.deltaTime * 10);
     }
 
-
+    // DEBUGGING
     void OnGUI ()
     {
         GUILayout.Label("TIME: " + Mathf.RoundToInt(timeDiveTotal).ToString() + "secs");
         GUILayout.Label("TOXICITY: " + nitrogen.ToString());
         GUILayout.Label("VELOCITY: " + GetComponent<Rigidbody2D>().velocity.y.ToString());
 
-        if (GUILayout.Button ("TREASURE!"))
+        if (GUILayout.Button ("Energy!"))
         {
-            weight += 10f;
+            energy += 10f;
         }
 
         if (GUILayout.Button("NITROGEN!"))
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,53 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager gm;
+
+    public List<GameObject> allTreasure;
+    [SerializeField]
+    private GameObject currentTreasure;
+
+    public delegate void OnTick();
+    public static event OnTick Tick;
+
+    public float tick = 1f;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+        if (gm == null) gm = this;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    if (Tick != null)
+        {
+            if (Time.time % tick <= Time.deltaTime) Tick();
+        }
+
+        foreach (GameObject t in allTreasure)
+        {
+            if (t)
+            {
+                if (Vector2.Distance(Diver.diver.transform.position, t.transform.position) < 0.75f)
+                {
+                    currentTreasure = t;
+                }
+                // currentTreasure = null;
+            }
+        }
+
+        if (currentTreasure != null)
+        {
+            Diver.diver.ReadyTreasure(currentTreasure);
+        }
 	}
+
+    public void GameObver ()
+    {
+        print("GameOver");
+    }
 }
